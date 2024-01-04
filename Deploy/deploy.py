5817453cda0af025c556cae3f0166338fa34dd8b
TN <amezaikupan@gmail.com>
2024-01-04 12:35:13 +0700

    Merged with tn

--- Deploy/deploy.py
+++ Deploy/deploy.py
@@ -6,7 +6,8 @@ import os
 
 class ApartmentPricePredictor:
     def __init__(self, model_path):
-        self.model = joblib.load(model_path)
+        path = os.path.dirname(__file__)
+        self.model = joblib.load(path + '/' + model_path)
 
     def predict_for_sale(self, data):
         return self.model.predict(data)
@@ -43,11 +44,12 @@ def main():
 
     df = pd.DataFrame(input_data)
 
-    predictor = ApartmentPricePredictor('decision_tree_model_for_sale.sav')
 
     if st.button('**Predict Price**'):
         if type == 'For sale':
             st.text('Predict price of apartment for sale')
+            predictor = ApartmentPricePredictor('decision_tree_model_for_sale.sav')
+
             price = predictor.predict_for_sale(df.values)
 
             if price[0] < 0:
@@ -56,7 +58,16 @@ def main():
                 st.text("The predicted price for the apartment is {:,} (VND)".format(np.round(price[0], 2)*1000000))
         elif type == 'For rent':
             st.text('Predict price of apartment for rent')
-            # TODO: Add model for rent
+
+            predictor = ApartmentPricePredictor('decision_tree_model_for_rent.sav')
+            price = predictor.predict_for_sale(df.values)
+
+            if price[0] < 0:
+                st.text("Cannot predict the price")
+            else:
+                st.text("The predicted price for the apartment is {:,} (VND)".format(np.round(price[0], 2)*1000000))
+       
+
 
 if __name__ == "__main__":
     main()
